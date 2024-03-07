3577ed0c8701a58f79e16e4120443b1c3cd82b5d
Stas Sergeev <stsp@users.sourceforge.net>
2024-03-07 13:29:54 +0300

    ministub: updates

--- ministub/ministub.c
+++ ministub/ministub.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include <fcntl.h>
 
 int DPMIQueryExtension(unsigned short *sel, unsigned short *off,
     const char *name)
@@ -18,43 +17,91 @@ int DPMIQueryExtension(unsigned short *sel, unsigned short *off,
   );
 }
 
-static void enter_stub(int fd, unsigned sel, unsigned off,
-    int argc, char *argv[], char *envp[])
+static void enter_stub(unsigned sel, unsigned off,
+    int argc, char *argv[], int envc, char *envp[], unsigned psp)
 {
   asm(
-    "mov eax, [ebp+8]\n"     // fd
-    "mov ecx, [ebp+20]\n"    // argc
-    "mov edx, [ebp+24]\n"    // argv
+    "mov eax, [ebp+32]\n"    // psp
+    "mov ecx, [ebp+16]\n"    // argc
+    "mov edx, [ebp+20]\n"    // argv
+    "mov ebx, [ebp+24]\n"    // envc
     "mov esi, [ebp+28]\n"    // envp
-    "push dword [ebp+12]\n"  // sel
-    "push dword [ebp+16]\n"  // off
+    "push dword [ebp+8]\n "  // sel
+    "push dword [ebp+12]\n"  // off
     "call far [ss:esp]\n"
     "add esp, 8\n"
   );
 }
 
-int main(int argc, char *argv[], char *envp[])
+extern void* __dpmi_psp;
+extern void* __dpmi_env;
+static unsigned psp;
+
+int main(int argc, char *argv[])
 {
   static const char *ext_nm = "DJ64STUB";
+  unsigned char *env = __dpmi_env;
+  char **envp;
   unsigned short sel, off;
-  int fd;
-  int err = DPMIQueryExtension(&sel, &off, ext_nm);
+  int envc, i, letter;
+  int err;
 
   if (argc == 0) {
     puts("no env");
     return 1;
   }
+  for (envc = i = letter = 0;; i++) {
+    if (env[i] == '\0') {
+      letter = 0;
+      if (env[i + 1] == '\0')
+        break;
+    } else {
+      if (!letter) {
+        letter = 1;
+        envc++;
+      }
+    }
+  }
+  envp = malloc((envc + 1) * sizeof(char *));
+  envp[envc] = NULL;
+  if (envc) {
+    int envc2;
+    for (envc2 = i = letter = 0;; i++) {
+      if (env[i] == '\0') {
+        letter = 0;
+        if (env[i + 1] == '\0')
+          break;
+      } else {
+        if (!letter) {
+          letter = 1;
+          envp[envc2++] = &env[i];
+        }
+      }
+    }
+  }
+  asm(
+    "mov eax, 0\n"
+    "mov ecx, 1\n"
+    "int 31h\n"
+    "mov [_psp], ax\n"
+    "mov eax, 7\n"
+    "mov bx, [_psp]\n"
+    "extern  ___dpmi_psp\n"
+    "mov cx, [___dpmi_psp + 2]\n"
+    "mov dx, [___dpmi_psp]\n"
+    "int 31h\n"
+    "mov eax, 8\n"
+    "mov bx, [_psp]\n"
+    "mov cx, 0\n"
+    "mov dx, 0ffh\n"
+    "int 31h\n"
+  );
+  err = DPMIQueryExtension(&sel, &off, ext_nm);
   if (err) {
     printf("%s unsupported (%x)\n", ext_nm, err);
     return 1;
   }
-  fd = open(argv[0], O_RDONLY);
-  if (fd == -1) {
-    printf("unable to open %s\n", argv[0]);
-    return 1;
-  }
-  enter_stub(fd, sel, off, argc, argv, envp);
-  close(fd);
+  enter_stub(sel, off, argc, argv, envc, envp, psp);
   puts("stub returned");
   return 0;
 }
