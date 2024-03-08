d161d872241d6bc25cd14e98d8d7c1fa70518bdc
Stas Sergeev <stsp@users.sourceforge.net>
2024-03-08 21:33:23 +0300

    its 2024 already

--- mini/ministub.c
+++ mini/ministub.c
@@ -1,6 +1,6 @@
 /*
  *  mini stub for dj64.
- *  Copyright (C) 2022,  stsp <stsp@users.sourceforge.net>
+ *  Copyright (C) 2024,  @stsp
  *
  *  This program is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
