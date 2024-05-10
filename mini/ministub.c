94f340f9fcb8c98caba758c9f17bc58e31f45617
Stas Sergeev <stsp@users.sourceforge.net>
2024-05-10 12:28:45 +0300

    Revert "ministub: pass shm flags to djstub"
    This reverts commit 5b86b4be2691900c007ec1d9f3dd2f604c7eb16f.

--- mini/ministub.c
+++ mini/ministub.c
@@ -18,12 +18,6 @@
  */
 #include <stdio.h>
 #include <string.h>
-#include <stdint.h>
-
-#define SHM_NOEXEC 1
-#define SHM_EXCL   2
-#define SHM_NEW_NS 4
-#define SHM_NS     8
 
 typedef struct
 {
@@ -66,8 +60,7 @@ int DPMIQueryExtension(unsigned short *sel, unsigned short *off,
 }
 
 static void enter_stub(unsigned sel, unsigned off,
-    int argc, char *argv[], int envc, char *envp[], unsigned psp,
-    unsigned flags)
+    int argc, char *argv[], int envc, char *envp[], unsigned psp)
 {
   asm(
     "mov eax, [ebp+32]\n"    // psp
@@ -75,7 +68,6 @@ static void enter_stub(unsigned sel, unsigned off,
     "mov edx, [ebp+20]\n"    // argv
     "mov ebx, [ebp+24]\n"    // envc
     "mov esi, [ebp+28]\n"    // envp
-    "mov edi, [ebp+36]\n"    // flags
     "push dword [ebp+8]\n "  // sel
     "push dword [ebp+12]\n"  // off
     "call far [ss:esp]\n"
@@ -96,8 +88,6 @@ int main(int argc, char *argv[])
   int envc, i, letter;
   int err;
   __dpmi_int_regs regs;
-  uint8_t shm_f0 = SHM_NOEXEC | SHM_EXCL | SHM_NEW_NS;
-  uint8_t shm_f1 = SHM_NOEXEC | SHM_EXCL | SHM_NS;
 
   if (argc == 0) {
     puts("no env");
@@ -160,7 +150,7 @@ int main(int argc, char *argv[])
   regs.ebx = 0x10;  // leave only PSP
   regs.es = (unsigned)__dpmi_psp >> 4;
   __dpmi_int(0x21, &regs);
-  enter_stub(sel, off, argc, argv, envc, envp, psp, shm_f0 | (shm_f1 << 8));
+  enter_stub(sel, off, argc, argv, envc, envp, psp);
   puts("stub returned");
   return 0;
 }
