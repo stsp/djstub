107758155ce90b8ec0f81d36f91771f7bdfac649
Stas Sergeev <stsp@users.sourceforge.net>
2024-06-20 11:48:01 +0300

    rework linking process
    Separate it out of stubification and off-load to -g switch.
    This allows to stop special-casing the first linking object.

--- stubify.c
+++ stubify.c
@@ -49,10 +49,7 @@ static int verbose;
 static int rmstub;
 #define MAX_OVL 5
 static char *overlay[MAX_OVL];
-static int noverlay;
-static const char *ovname;
 static int strip;
-static uint16_t stub_flags;
 static const uint8_t stub_ver = 4;
 
 static int copy_file(const char *ovl, int ofile)
@@ -209,7 +206,7 @@ static int coff2exe(const char *fname, const char *oname, int info)
         memcpy(&coff_file_size, &buf[0x1c], sizeof(coff_file_size));
         memcpy(mzhdr_buf, buf, sizeof(mzhdr_buf));
         can_copy_ovl++;
-        if (rmstub || noverlay || strip)
+        if (rmstub || strip)
           rmoverlay++;
 
         if (info)
@@ -238,10 +235,11 @@ static int coff2exe(const char *fname, const char *oname, int info)
             cnt++;
           }
           if (buf[0x3b] >= 4 && buf[0x3b] < 0x20 && buf[0x2e]) {
-            memcpy(&stub_flags, buf + 0x2c, 2);
-            IPRINTF("Overlay name: %s\n", buf + 0x2e);
+            uint16_t flags;
+            memcpy(&flags, buf + 0x2c, 2);
+            IPRINTF("Overlay name: %.12s\n", buf + 0x2e);
             IPRINTF("Stub version: %i\n", buf[0x3b]);
-            IPRINTF("Stub flags: 0x%04x\n", stub_flags);
+            IPRINTF("Stub flags: 0x%04x\n", flags);
           }
         }
       }
@@ -287,33 +285,13 @@ static int coff2exe(const char *fname, const char *oname, int info)
   if (!coff_file_size)
     coff_file_size = lseek(ifile, 0, SEEK_END) - coffset;
   lseek(ifile, coffset, SEEK_SET);
-  if (noverlay) {
-    struct stat sb;
-    /* store overlay sizes in overlay info */
-    memcpy(_binary_stub_exe_start + 0x1c, &coff_file_size,
-          sizeof(coff_file_size));
-    for (i = 0; i < noverlay; i++) {
-      int rc = stat(overlay[i], &sb);
-      if (rc == -1) {
-        fprintf(stderr, "failed to stat %s: %s\n", overlay[i],
-            strerror(errno));
-        close(ifile);
-        return -1;
-      }
-      memcpy(_binary_stub_exe_start + 0x20 + i * 4, &sb.st_size, 4);
-    }
-  } else if (can_copy_ovl) {
+  if (can_copy_ovl) {
     /* copy entire overlay info except 0x3c */
     memcpy(_binary_stub_exe_start + 0x1c, mzhdr_buf + 0x1c, 0x3c - 0x1c);
   } else {
     /* assume there is just 1 overlay */
     memcpy(_binary_stub_exe_start + 0x1c, &coff_file_size,
           sizeof(coff_file_size));
-  }
-  memcpy(_binary_stub_exe_start + 0x2c, &stub_flags, sizeof(stub_flags));
-  if (ovname) {
-    strncpy(_binary_stub_exe_start + 0x2e, ovname, 12);
-    _binary_stub_exe_start[0x2e + 12] = '\0';
   }
   _binary_stub_exe_start[0x3b] = stub_ver;
   memcpy(_binary_stub_exe_start + 0x3c, &stub_size, sizeof(stub_size));
@@ -366,18 +344,6 @@ static int coff2exe(const char *fname, const char *oname, int info)
     }
   }
   close(ifile);
-
-  for (i = 0; i < noverlay; i++)
-  {
-    int rc = copy_file(overlay[i], ofile);
-    if (rc < 0)
-    {
-      fprintf(stderr, "failed to copy overlays\n");
-      close(ofile);
-      unlink(oname);
-      return -1;
-    }
-  }
   close(ofile);
 
   if (!oname)
@@ -389,8 +355,7 @@ static int coff2exe(const char *fname, const char *oname, int info)
 static void print_help(void)
 {
   fprintf(stderr, "Usage: stubify [-v] [-l <overlay>] [-o <out_file>] <program>\n\n"
-	  "<program> may be COFF or stubbed .exe, and may be COFF with .exe extension.\n"
-	  "Resulting file will have .exe\n\n"
+	  "<program> may be COFF or stubbed .exe.\n\n"
 	  "Options:\n"
 	  "-v -> verbose\n"
 	  "-i -> display file info\n"
@@ -400,7 +365,7 @@ static void print_help(void)
 	  "-n <name> -> write <name> into an overlay info\n"
 	  "-o <name> -> write output into <name>\n"
 	  "-f <flags> -> write <flags> into an overlay info\n"
-	  "-g <file_name> -> write a stub alone into a file\n"
+	  "-g -> generate a new file\n"
 	  "\nNote: -g is useful only for debugging, as the stub is being\n"
 	  "customized for a particular program and its overlay.\n"
   );
@@ -408,12 +373,15 @@ static void print_help(void)
 
 int main(int argc, char **argv)
 {
-  char *generate = NULL;
+  int generate = 0;
   char *oname = NULL;
   int info = 0;
   int c;
+  int noverlay = 0;
+  const char *ovname = NULL;
+  uint16_t stub_flags = 0;
 
-  while ((c = getopt(argc, argv, "virsg:l:o:n:f:")) != -1)
+  while ((c = getopt(argc, argv, "virsgl:o:n:f:")) != -1)
   {
     switch (c) {
     case 'v':
@@ -429,7 +397,7 @@ int main(int argc, char **argv)
       strip = 1;
       break;
     case 'g':
-      generate = optarg;
+      generate = 1;
       break;
     case 'l':
       assert(noverlay < MAX_OVL);
@@ -455,18 +423,58 @@ int main(int argc, char **argv)
   if (generate)
   {
     int ofile;
+    int i;
+    const uint32_t stub_size = _binary_stub_exe_end - _binary_stub_exe_start;
 
-    ofile = open(generate, O_WRONLY|O_CREAT|O_TRUNC, 0666);
+    if (!oname) {
+      fprintf(stderr, "djstubify: -o missing\n");
+      return EXIT_FAILURE;
+    }
+    if (noverlay) {
+      struct stat sb;
+      /* store overlay sizes in overlay info */
+      for (i = 0; i < noverlay; i++) {
+        int rc = stat(overlay[i], &sb);
+        if (rc == -1) {
+          fprintf(stderr, "failed to stat %s: %s\n", overlay[i],
+              strerror(errno));
+          return EXIT_FAILURE;
+        }
+        memcpy(_binary_stub_exe_start + 0x1c + i * 4, &sb.st_size, 4);
+      }
+    }
+    memcpy(_binary_stub_exe_start + 0x2c, &stub_flags, sizeof(stub_flags));
+    if (ovname) {
+      strncpy(_binary_stub_exe_start + 0x2e, ovname, 12);
+      _binary_stub_exe_start[0x2e + 12] = '\0';
+    }
+    _binary_stub_exe_start[0x3b] = stub_ver;
+    memcpy(_binary_stub_exe_start + 0x3c, &stub_size, sizeof(stub_size));
+
+    ofile = open(oname, O_WRONLY | O_CREAT | O_TRUNC, 0644);
     if (ofile < 0)
     {
       fprintf(stderr, "Cannot open output file to generate\n");
       perror("open()");
       return 1;
     }
-    v_printf("stubify: generate %s\n", generate);
+    v_printf("stubify: generate %s\n", oname);
 
     write(ofile, _binary_stub_exe_start,
         _binary_stub_exe_end-_binary_stub_exe_start);
+
+    for (i = 0; i < noverlay; i++)
+    {
+      int rc = copy_file(overlay[i], ofile);
+      if (rc < 0)
+      {
+        fprintf(stderr, "failed to copy overlays\n");
+        close(ofile);
+        unlink(oname);
+        return EXIT_FAILURE;
+      }
+    }
+
     close(ofile);
   }
   else
