35ff935c8d377bbe11e6df3ac9ba2b99cd06afd7
Stas Sergeev <stsp@users.sourceforge.net>
2026-06-24 18:20:15 +0300

    stub: port to smallerc

--- full/stub.c
+++ full/stub.c
@@ -22,7 +22,11 @@
 #include <unistd.h>
 #include <stdint.h>
 #include <fcntl.h>
+#ifdef __GNUC__
 #include <dpmi.h>
+#else
+#include "dpmi.h"
+#endif
 #include <assert.h>
 #include "stubinfo.h"
 #include "coff.h"
@@ -48,22 +52,159 @@ static char_far client_memory;
 static DPMI_FP clnt_entry;
 static _GO32_StubInfo stubinfo;
 
+#ifndef __GNUC__
+typedef struct
+{
+  unsigned long  edi;
+  unsigned long  esi;
+  unsigned long  ebp;
+  unsigned long  resrvd;
+  unsigned long  ebx;
+  unsigned long  edx;
+  unsigned long  ecx;
+  unsigned long  eax;
+  unsigned short flags;
+  unsigned short es;
+  unsigned short ds;
+  unsigned short fs;
+  unsigned short gs;
+  unsigned short ip;
+  unsigned short cs;
+  unsigned short sp;
+  unsigned short ss;
+} __dpmi_int_regs;
+
+typedef struct {
+    uint16_t rm, pm;
+} dpmi_dos_block;
+#endif
+
 static void link_umb(int on)
 {
+#ifdef __GNUC__
     asm volatile("int $0x21\n"
         :
         : "a"(0x5803), "b"(on)
         : "cc");
+#else
+    asm(
+        "mov eax, 0x5803\n"
+        "mov ebx, [ebp + 8]\n"
+        "int 0x21\n"
+    );
+#endif
     if (on) {
+#ifdef __GNUC__
         asm volatile("int $0x21\n"
             :
             : "a"(0x5801), "b"(0x80)
             : "cc");
+#else
+        asm(
+            "mov eax, 0x5801\n"
+            "mov ebx, 0x80\n"
+            "int 0x21\n"
+        );
+#endif
     }
 }
 
+#ifndef __GNUC__
+extern void* __dpmi_psp;
+extern void* __dpmi_env;
+
+static unsigned short alloc_psp(void)
+{
+  asm(
+    "mov eax, 0\n"    // alloc desc for PSP
+    "mov ecx, 1\n"    // 1 desc
+    "int 31h\n"
+    "push ax\n"       // psp
+    "mov eax, 7\n"    // set base
+    "pop bx\n"
+    "push bx\n"
+    "extern  ___dpmi_psp\n"
+    "mov cx, [___dpmi_psp + 2]\n"
+    "mov dx, [___dpmi_psp]\n"
+    "int 31h\n"
+    "mov eax, 8\n"    // set limit
+    "pop bx\n"
+    "push bx\n"
+    "mov cx, 0\n"
+    "mov dx, 0ffh\n"  // to PSP size
+    "int 31h\n"
+    "pop ax\n"
+  );
+}
+
+static unsigned lcall(unsigned sel, unsigned off, unsigned ax)
+{
+  asm(
+    "push es\n"
+    "push dword [ebp+8]\n "  // sel
+    "push dword [ebp+12]\n"  // off
+    "mov eax, [ebp+16]\n"    // ax
+    "call far [ss:esp]\n"
+    "pop eax\n"
+    "pop eax\n"
+    "pushf\n"
+    "pop eax\n"
+    "mov [_psp_sel], es\n"
+    "pop es\n"
+  );
+}
+
+static void int2f(__dpmi_int_regs *r)
+{
+  asm(
+    "push es\n"
+    "mov ebx, [ebp+8]\n"
+    "mov edi, [ebx]\n"
+    "mov esi, [ebx+4]\n"
+    "mov edx, [ebx+20]\n"
+    "mov ecx, [ebx+24]\n"
+    "mov eax, [ebx+28]\n"
+    "mov ebx, [ebx+16]\n"
+    "stc\n"
+    "int 0x2f\n"
+    "push ebx\n"
+    "mov ebx, [ebp+8]\n"
+    "pop dword [ebx+16]\n"
+    "pushfw\n"
+    "pop word [ebx+32]\n"
+    "mov [ebx], edi\n"
+    "mov [ebx+4], esi\n"
+    "mov [ebx+20], edx\n"
+    "mov [ebx+24], ecx\n"
+    "mov [ebx+28], eax\n"
+    "mov [ebx+34], es\n"
+    "mov [ebx+36], ds\n"
+    "mov [ebx+38], fs\n"
+    "mov [ebx+40], gs\n"
+    "pop es\n"
+  );
+}
+
+static unsigned short __my_ds(void)
+{
+  asm("mov ax, ds\n");
+}
+
+static void jump_to_entry(unsigned stubinfo_fs, unsigned clnt_ds)
+{
+  asm(
+    "mov fs, [ebp + 8]\n"
+    "push ds\n"
+    "pop es\n"
+    "mov ds, [ebp + 12]\n"
+    "jmp far dword [es:_clnt_entry]\n"
+  );
+}
+#endif
+
 static void dpmi_init(void)
 {
+#ifdef __GNUC__
     union REGPACK r = {};
     char_far sw;
     unsigned mseg = 0, f;
@@ -110,6 +251,32 @@ static void dpmi_init(void)
         fprintf(stderr, "DPMI init failed\n");
         exit(EXIT_FAILURE);
     }
+#else
+    __dpmi_int_regs r = {0};
+    unsigned f;
+
+#define CF 1
+    r.eax = 0x1687;
+    int2f(&r);
+    if ((r.flags & CF) || r.eax != 0) {
+        stub_debug("DPMI unavailable\n");
+        psp_sel = alloc_psp();
+        return;
+    }
+    if (!(r.ebx & 1)) {
+        fprintf(stderr, "DPMI-32 unavailable\n");
+        exit(EXIT_FAILURE);
+    }
+    if (r.esi) {
+        fprintf(stderr, "invalid DPMI server, %i para requested\n", r.esi);
+        exit(EXIT_FAILURE);
+    }
+    f = lcall(r.es, r.edi, 1);
+    if (f & CF) {
+        fprintf(stderr, "DPMI init failed\n");
+        exit(EXIT_FAILURE);
+    }
+#endif
 }
 
 static const char *_basename(const char *name)
@@ -265,25 +432,43 @@ int main(int argc, char *argv[], char *envp[])
     /* DJGPP relies on ds_selector, cs_selector and ds_segment all mapping
      * the same real-mode memory block. */
     link_umb(1);
+#ifdef __GNUC__
     db = _DPMIAllocateDOSMemoryBlock(stubinfo.minkeep >> 4);
+#else
+    db.rm = dpmi_allocate_dos_memory(stubinfo.minkeep >> 4, &db.pm);
+#endif
     link_umb(0);
     stub_debug("rm seg 0x%x\n", db.rm);
     stubinfo.ds_selector = db.pm;
     stubinfo.ds_segment = db.rm;
     /* create alias */
+#ifdef __GNUC__
     asm volatile("int $0x31\n"
         : "=a"(stubinfo.cs_selector)
         : "a"(0xa), "b"(db.pm));
+#else
+    stubinfo.cs_selector = dpmi_create_alias_descriptor(db.pm);
+#endif
     /* set descriptor access rights */
+#ifdef __GNUC__
     asm volatile("int $0x31\n"
         :
         : "a"(9), "b"(stubinfo.cs_selector), "c"(0x00fb)
         : "cc");
+#else
+    dpmi_set_descriptor_access_rights(stubinfo.cs_selector, 0xfb);
+#endif
 
+#ifdef __GNUC__
     clnt_entry.selector = _DPMIAllocateLDTDescriptors(1);
     clnt_ds = _DPMIAllocateLDTDescriptors(1);
+#else
+    clnt_entry.selector = dpmi_allocate_ldt_descriptors(1);
+    clnt_ds = dpmi_allocate_ldt_descriptors(1);
+#endif
     alloc_size = PAGE_ALIGN(stubinfo.initial_size);
     /* allocate mem */
+#ifdef __GNUC__
     asm volatile("int $0x31\n"
         : "=b"(mem_hi), "=c"(mem_lo), "=S"(si), "=D"(di)
         : "a"(0x501),
@@ -292,11 +477,24 @@ int main(int argc, char *argv[], char *envp[])
         : "cc");
     stubinfo.memory_handle = ((uint32_t)si << 16) | di;
     mem_lin = ((uint32_t)mem_hi << 16) | mem_lo;
+#else
+    rc = dpmi_allocate_memory(alloc_size, &mem_lin, &stubinfo.memory_handle);
+    if (rc == -1) {
+        stub_debug("dpmi mem alloc failed\n");
+        exit(EXIT_FAILURE);
+    }
+#endif
     mem_base = mem_lin - va;
     stubinfo.mem_base = mem_base;
     stub_debug("mem_lin 0x%lx mem_base 0x%lx\n", mem_lin, mem_base);
+#ifdef __GNUC__
     client_memory = __MK_FP(clnt_ds, 0);
+#else
+    __FP_SEG(client_memory) = clnt_ds;
+    __FP_OFF(client_memory) = 0;
+#endif
     /* set base */
+#ifdef __GNUC__
     asm volatile("int $0x31\n"
         :
         : "a"(7),
@@ -304,19 +502,31 @@ int main(int argc, char *argv[], char *envp[])
           "c"((uint16_t)(mem_base >> 16)),
           "d"((uint16_t)(mem_base & 0xffff))
         : "cc");
+#else
+    dpmi_set_segment_base_address(clnt_entry.selector, mem_base);
+#endif
     /* set descriptor access rights */
+#ifdef __GNUC__
     asm volatile("int $0x31\n"
         :
         : "a"(9), "b"(clnt_entry.selector), "c"(0xc0fb)
         : "cc");
+#else
+    dpmi_set_descriptor_access_rights(clnt_entry.selector, 0xc0fb);
+#endif
     /* set limit */
+#ifdef __GNUC__
     asm volatile("int $0x31\n"
         :
         : "a"(8), "b"(clnt_entry.selector),
           "c"(0xffff),
           "d"(0xffff)
         : "cc");
+#else
+    dpmi_set_segment_limit(clnt_entry.selector, 0xffffffff);
+#endif
     /* set base */
+#ifdef __GNUC__
     asm volatile("int $0x31\n"
         :
         : "a"(7),
@@ -324,32 +534,41 @@ int main(int argc, char *argv[], char *envp[])
           "c"((uint16_t)(mem_base >> 16)),
           "d"((uint16_t)(mem_base & 0xffff))
         : "cc");
+#else
+    dpmi_set_segment_base_address(clnt_ds, mem_base);
+#endif
     /* set descriptor access rights */
+#ifdef __GNUC__
     asm volatile("int $0x31\n"
         :
         : "a"(9), "b"(clnt_ds), "c"(0xc0f3)
         : "cc");
+#else
+    dpmi_set_descriptor_access_rights(clnt_ds, 0xc0f3);
+#endif
     /* set limit */
+#ifdef __GNUC__
     asm volatile("int $0x31\n"
         :
         : "a"(8), "b"(clnt_ds),
           "c"(0xffff),
           "d"(0xffff)
         : "cc");
+#else
+    dpmi_set_segment_limit(clnt_ds, 0xffffffff);
+#endif
 
     /* create ds alias */
+#ifdef __GNUC__
     asm volatile(
         "mov %%ds, %%bx\n"
         "int $0x31\n"
         : "=a"(stubinfo_fs)
         : "a"(0xa));
-    /* set limit */
-    asm volatile("int $0x31\n"
-        :
-        : "a"(8), "b"(stubinfo_fs),
-          "c"(0),
-          "d"(sizeof(_GO32_StubInfo) - 1)
-        : "cc");
+#else
+    stubinfo_fs = dpmi_create_alias_descriptor(__my_ds());
+#endif
+#ifdef __GNUC__
     stubinfo_mem = _DPMIGetSegmentBaseAddress(stubinfo_fs) +
             (uintptr_t)&stubinfo;
     mem_hi = stubinfo_mem >> 16;
@@ -359,6 +578,22 @@ int main(int argc, char *argv[], char *envp[])
         :
         : "a"(7), "b"(stubinfo_fs), "c"(mem_hi), "d"(mem_lo)
         : "cc");
+#else
+    dpmi_get_segment_base_address(stubinfo_fs, &stubinfo_mem);
+    stubinfo_mem += (uintptr_t)&stubinfo;
+    dpmi_set_segment_base_address(stubinfo_fs, stubinfo_mem);
+#endif
+    /* set limit */
+#ifdef __GNUC__
+    asm volatile("int $0x31\n"
+        :
+        : "a"(8), "b"(stubinfo_fs),
+          "c"(0),
+          "d"(sizeof(_GO32_StubInfo) - 1)
+        : "cc");
+#else
+    dpmi_set_segment_limit(stubinfo_fs, sizeof(_GO32_StubInfo) - 1);
+#endif
 
     ops->read_sections(handle, client_memory, ifile, coffset);
 
@@ -377,6 +612,7 @@ int main(int argc, char *argv[], char *envp[])
     stubinfo.stubinfo_ver |= 2;
 
     stub_debug("Jump to entry...\n");
+#ifdef __GNUC__
     asm volatile(
           ".arch i386\n"
           "mov %0, %%fs\n"
@@ -388,5 +624,8 @@ int main(int argc, char *argv[], char *envp[])
         :
         : "a"(stubinfo_fs), "r"(clnt_ds), "m"(clnt_entry)
         : "memory");
+#else
+    jump_to_entry(stubinfo_fs, clnt_ds);
+#endif
     return 0;
 }
